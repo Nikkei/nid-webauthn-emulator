63e1c9266a007ce129af6cbfeeae06f172b7888a
Makoto Fuchiwaki <phwinx@gmail.com>
2024-06-21 14:29:20 +0900

    feat: EmulatorのJSONインターフェースの追加

--- spec/integration/integration.spec.ts
+++ spec/integration/integration.spec.ts
@@ -1,6 +1,5 @@
 import { describe, test } from "@jest/globals";
 import WebAuthnApiEmulator from "../../src/index";
-import { parseCreationOptionsFromJSON, parseRequestOptionsFromJSON } from "../../src/webauthn/webauthn-model-json";
 import { WebAuthnIO } from "./webauthn-io";
 
 describe("Passkeys Integration Test by webauthn.io", () => {
@@ -10,19 +9,19 @@ describe("Passkeys Integration Test by webauthn.io", () => {
     const webauthnIO = await WebAuthnIO.create();
 
     // Create passkey.
-    const creationOptions = { publicKey: parseCreationOptionsFromJSON(await webauthnIO.getRegistrationOptions()) };
+    const creationOptions = await webauthnIO.getRegistrationOptions();
     console.log("Registration options", creationOptions);
-    const creationCredential = await webAuthnApiEmulator.create(origin, creationOptions);
-    console.log("Registration credential", creationCredential.toJSON());
-    await webauthnIO.getRegistrationVerification(creationCredential.toJSON());
+    const creationCredential = webAuthnApiEmulator.createJSON(origin, creationOptions);
+    console.log("Registration credential", creationCredential);
+    await webauthnIO.getRegistrationVerification(creationCredential);
     console.log("Registration verification completed");
 
     // Authenticate passkey.
-    const requestOptions = { publicKey: parseRequestOptionsFromJSON(await webauthnIO.getAuthenticationOptions()) };
+    const requestOptions = await webauthnIO.getAuthenticationOptions();
     console.log("Authentication options", requestOptions);
-    const requestCredential = await webAuthnApiEmulator.get(origin, requestOptions);
-    console.log("Authentication credential", requestCredential.toJSON());
-    await webauthnIO.getAuthenticationVerification(requestCredential.toJSON());
+    const requestCredential = webAuthnApiEmulator.getJSON(origin, requestOptions);
+    console.log("Authentication credential", requestCredential);
+    await webauthnIO.getAuthenticationVerification(requestCredential);
     console.log("Authentication verification completed");
   }, 60000);
 });
